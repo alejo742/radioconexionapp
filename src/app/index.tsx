41310f830e7f07fc9c6e830532df45a370970cc9
Alejandro Manrique <alejandromanriquenunez@gmail.com>
2025-05-04 17:51:47 -0400

    fix: remove unused dependency

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, Text, View, Alert } from 'react-native';
 
 export default function App() {
